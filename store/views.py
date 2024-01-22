7f2dcaf1c2c82998ac8543882a7828f433916dd2
luyanakat <cuong02.master@gmail.com>
2024-01-22 16:52:44 +0700

    update module detail product/init category

--- store/views.py
+++ store/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+from django.contrib.humanize.templatetags.humanize import intcomma
+from django.shortcuts import render, get_object_or_404
+
+from category.models import Category
+from store.models import Product
+
 
 # Create your views here.
+def store(request, category_slug=None):
+    categories = None
+    products = None
+
+    if category_slug is not None:
+        categories = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.filter(category=categories, is_available=True)
+        product_count = products.count()
+    else:
+        products = Product.objects.all().filter(is_available=True)
+        product_count = products.count()
+
+    formatted_products = [
+        {
+            'price': intcomma("{:.0f}".format(product.price)),
+            'image': product.image,
+            'product_name': product.product_name,
+            'get_url': product.get_url(),
+        }
+        for product in products
+    ]
+    context = {
+        'products': formatted_products,
+        'product_count': product_count,
+    }
+    return render(request, 'store/store.html', context)
+
+
+def product_detail(request, category_slug, product_slug):
+    try:
+        single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+    except Exception as e:
+        raise e
+    formatted_product = {
+        'price': intcomma("{:.0f}".format(single_product.price)),
+        'image': single_product.image,
+        'product_name': single_product.product_name,
+        'description': single_product.description,
+        'slug': single_product.slug,
+        'get_url': single_product.get_url(),
+        'stock': single_product.stock,
+    }
+    context = {
+        'single_product': formatted_product
+    }
+    return render(request, 'store/product_detail.html', context)
--- webshop/views.py
+++ webshop/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
 from store.models import Product
+from django.contrib.humanize.templatetags.humanize import intcomma
 
 
 def home(request):
     products = Product.objects.all().filter(is_available=True)
-
+    formatted_products = [
+        {
+            'price': intcomma("{:.0f}".format(product.price)),
+            'image': product.image,
+            'product_name': product.product_name,
+            'get_url': product.get_url(),
+            'description': product.description,
+            'stock': product.stock,
+        }
+        for product in products
+    ]
     context = {
-        'products': products,
+        'products': formatted_products,
     }
+    print(formatted_products)
     return render(request, 'home.html', context)
